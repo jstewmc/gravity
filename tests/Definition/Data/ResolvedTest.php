fdc41b02088897a810203ab722865d35a61dc031
Jack Clayton <clayjs0@gmail.com>
2019-01-20 13:24:20 -0700

    Add tests for isService() and isSetting() methods

--- tests/Definition/Data/ResolvedTest.php
+++ tests/Definition/Data/ResolvedTest.php
@@ -6,7 +6,7 @@
 
 namespace Jstewmc\Gravity\Definition\Data;
 
-use Jstewmc\Gravity\Id\Data\Id;
+use Jstewmc\Gravity\Id\Data as Id;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -16,7 +16,7 @@ class ResolvedTest extends TestCase
 {
     public function testGetKey(): void
     {
-        $id = $this->createMock(Id::class);
+        $id = $this->createMock(Id\Id::class);
 
         $this->assertSame($id, (new Resolved($id))->getKey());
 
@@ -27,7 +27,7 @@ class ResolvedTest extends TestCase
     {
         $segments = ['foo', 'bar', 'baz'];
 
-        $id = $this->createMock(Id::class);
+        $id = $this->createMock(Id\Id::class);
         $id->method('getSegments')->willReturn($segments);
 
         $this->assertEquals($segments, (new Resolved($id))->getSegments());
@@ -35,16 +35,34 @@ class ResolvedTest extends TestCase
 
     public function testGetValue(): void
     {
-        $id = $this->createMock(Id::class);
+        $id = $this->createMock(Id\Id::class);
 
         $this->assertNull((new Resolved($id))->getValue());
 
         return;
     }
 
+    public function testIsService(): void
+    {
+        $id = $this->createMock(Id\Service::class);
+
+        $definition = new Resolved($id);
+
+        $this->assertTrue($definition->isService());
+    }
+
+    public function testIsSetting(): void
+    {
+        $id = $this->createMock(Id\Setting::class);
+
+        $definition = new Resolved($id);
+
+        $this->assertTrue($definition->isSetting());
+    }
+
     public function testSetValue(): void
     {
-        $id = $this->createMock(Id::class);
+        $id = $this->createMock(Id\Id::class);
 
         $definition = new Resolved($id);
 
