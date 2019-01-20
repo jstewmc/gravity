62ac06485a17d3c28b85130477e7ae3831b44ade
Jack Clayton <clayjs0@gmail.com>
2019-01-20 13:17:52 -0700

    Refactor Factory

--- src/Factory.php
+++ src/Factory.php
@@ -1,8 +1,5 @@
 <?php
 /**
- * The file for the factory interface
- *
- * @author     Jack Clayton <clayjs0@gmail.com>
  * @copyright  2018 Jack Clayton
  * @license    MIT
  */
@@ -11,19 +8,7 @@ namespace Jstewmc\Gravity;
 
 use Jstewmc\Gravity\Manager;
 
-/**
- * The factory interface
- *
- * @since  0.1.0
- */
 interface Factory
 {
-    /**
-     * Called when the factory is treated like a function
-     *
-     * @param   Manager  $g  the Gravity manager
-     * @return  object
-     * @since   0.1.0
-     */
     public function __invoke(Manager $g);
 }
