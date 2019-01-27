c210d19d12ec3e736cc4f609dde8aa1c14902b2f
Jack Clayton <clayjs0@gmail.com>
2019-01-26 20:09:31 -0700

    Add close-namespace service

---
+++ tests/Ns/Service/CloseTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @copyright  2018 Jack Clayton
+ * @license    MIT
+ */
+
+namespace Jstewmc\Gravity\Ns\Service;
+
+use Jstewmc\Gravity\Ns\Data\{Closed, Opened};
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group  namespace
+ */
+class CloseTest extends TestCase
+{
+    public function testInvoke(): void
+    {
+        $expected = new Closed();
+        $actual   = (new Close())(new Opened());
+
+        $this->assertEquals($expected, $actual);
+    }
+}
