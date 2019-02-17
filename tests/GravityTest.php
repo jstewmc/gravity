e2d740497abd00469f03cce41cf49a22aee91b53
javabudd <javabudd@gmail.com>
2019-02-17 09:20:35 -0600

    Add Cache alias

--- tests/GravityTest.php
+++ tests/GravityTest.php
@@ -8,7 +8,7 @@ namespace Jstewmc\Gravity;
 
 use PHPUnit\Framework\TestCase;
 use Psr\Log;
-use Psr\SimpleCache\CacheInterface;
+use Psr\SimpleCache\CacheInterface as Cache;
 
 class GravityTest extends TestCase
 {
@@ -30,7 +30,7 @@ class GravityTest extends TestCase
     {
         $gravity = new Gravity();
 
-        $cache = $this->createMock(CacheInterface::class);
+        $cache = $this->createMock(Cache::class);
 
         $this->assertSame($gravity, $gravity->setCache($cache));
     }
