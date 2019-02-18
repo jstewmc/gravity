3e911f36d546d01c9464e5383a1bbe1269348475
Jack Clayton <clayjs0@gmail.com>
2019-02-18 09:46:33 -0700

    Add validate-project service

---
+++ src/Project/Service/Validate.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * @copyright  2018 Jack Clayton
+ * @license    MIT
+ */
+
+namespace Jstewmc\Gravity\Project\Service;
+
+use Jstewmc\Gravity\Manager\Data\Manager;
+use Jstewmc\Gravity\Project\Data\Project;
+use Jstewmc\Gravity\Service\Data\Service;
+use Jstewmc\Gravity\Service\Service\Instantiate;
+use Jstewmc\Gravity\Exception;
+
+class Validate
+{
+    private $instantiate;
+
+    public function __construct(Instantiate $instantiate)
+    {
+        $this->instantiate = $instantiate;
+    }
+
+    public function __invoke(Project $project, Manager $g): array
+    {
+        return array_merge(
+            $this->validateRequirements($project),
+            $this->validateServices($project, $g)
+        );
+    }
+
+    private function instantiate(Service $service, Manager $g): object
+    {
+        return ($this->instantiate)($service, $g);
+    }
+
+    private function validateRequirements(Project $project): array
+    {
+        $errors = [];
+
+        foreach ($project->getRequirements() as $requirement) {
+            if ($requirement->isService()) {
+                if (!$project->hasService($requirement->getKey())) {
+                    $errors[(string)$requirement->getKey()] = "Required "
+                        . "service, '{$requirement->getKey()}', not found";
+                }
+            } else {
+                if (!$project->hasSetting($requirement->getKey())) {
+                    $errors[(string)$requirement->getKey()] = "Required "
+                        . "setting, '{$requirement->getKey()}', not found";
+                }
+            }
+        }
+
+        return $errors;
+    }
+
+    private function validateServices(Project $project, Manager $g): array
+    {
+        $errors = [];
+
+        foreach ($project->getServices() as $service) {
+            try {
+                $this->instantiate($service, $g);
+            } catch (Exception $e) {
+                $errors[(string)$service->getId()] = "Instatiating service, "
+                    . "'{$service->getId()}', failed with " . get_class($e)
+                    . ": " . $e->getMessage();
+            }
+        }
+
+        return $errors;
+    }
+}
---
+++ tests/Project/Service/ValidateTest.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * @copyright  2018 Jack Clayton
+ * @license    MIT
+ */
+
+namespace Jstewmc\Gravity\Project\Service;
+
+use Jstewmc\Gravity\Exception;
+use Jstewmc\Gravity\Id\Data\{
+    Id,
+    Service as ServiceId,
+    Setting as SettingId
+};
+use Jstewmc\Gravity\Manager\Data\Manager;
+use Jstewmc\Gravity\Project\Data\Project;
+use Jstewmc\Gravity\Requirement\Data\Resolved as Requirement;
+use Jstewmc\Gravity\Service\Data\Service;
+use Jstewmc\Gravity\Service\Service\Instantiate;
+use PHPUnit\Framework\TestCase;
+
+class ValidateTest extends TestCase
+{
+    private $manager;
+
+    public function setUp(): void
+    {
+        $this->manager = $this->createMock(Manager::class);
+    }
+
+    public function testInvokeIfProjectIsValid(): void
+    {
+        $project = $this->createMock(Project::class);
+        $project->method('getRequirements')->willReturn([]);
+        $project->method('getServices')->willReturn([]);
+
+        $instantiate = $this->createMock(Instantiate::class);
+
+        $sut = new Validate($instantiate);
+
+        $this->assertEmpty($sut($project, $this->manager));
+    }
+
+    public function testInvokeIfServiceRequirementIsInvalid(): void
+    {
+        $key = 'foo\bar\baz';
+
+        $id = $this->createMock(ServiceId::class);
+        $id->method('__toString')->willReturn($key);
+
+        $requirement = $this->createMock(Requirement::class);
+        $requirement->method('isService')->willReturn(true);
+        $requirement->method('getKey')->willReturn($id);
+
+        $project = $this->createMock(Project::class);
+        $project->method('getRequirements')->willReturn([$requirement]);
+        $project->method('getServices')->willReturn([]);
+        $project->method('hasService')->willReturn(false);
+
+        $instantiate = $this->createMock(Instantiate::class);
+
+        $sut = new Validate($instantiate);
+
+        $this->assertArrayHasKey($key, $sut($project, $this->manager));
+    }
+
+    public function testInvokeIfSettingRequirementIsInvalid(): void
+    {
+        $key = 'foo.bar.baz';
+
+        $id = $this->createMock(SettingId::class);
+        $id->method('__toString')->willReturn($key);
+
+        $requirement = $this->createMock(Requirement::class);
+        $requirement->method('isService')->willReturn(false);
+        $requirement->method('getKey')->willReturn($id);
+
+        $project = $this->createMock(Project::class);
+        $project->method('getRequirements')->willReturn([$requirement]);
+        $project->method('getServices')->willReturn([]);
+        $project->method('hasSetting')->willReturn(false);
+
+        $instantiate = $this->createMock(Instantiate::class);
+
+        $sut = new Validate($instantiate);
+
+        $this->assertArrayHasKey($key, $sut($project, $this->manager));
+    }
+
+    public function testInvokeIfServiceDefinitionIsInvalid(): void
+    {
+        $key = 'foo\bar\baz';
+
+        $id = $this->createMock(ServiceId::class);
+        $id->method('__toString')->willReturn($key);
+
+        $service = $this->createMock(Service::class);
+        $service->method('getId')->willReturn($id);
+
+        $project = $this->createMock(Project::class);
+        $project->method('getRequirements')->willReturn([]);
+        $project->method('getServices')->willReturn([$service]);
+
+        $e = $this->createMock(Exception::class);
+
+        $instantiate = $this->createMock(Instantiate::class);
+        $instantiate->method('__invoke')->will($this->throwException($e));
+
+        $sut = new Validate($instantiate);
+
+        $this->assertArrayHasKey($key, $sut($project, $this->manager));
+    }
+}
